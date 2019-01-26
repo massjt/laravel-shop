8abee09c1a149718b5750963b0c6ea06f35461ea
massjt <massjt@qq.com>
2019-01-26 04:52:12 +0100

    触发激活邮件

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        Registered::class => [
+            RegisteredListener::class,
         ],
     ];
 
